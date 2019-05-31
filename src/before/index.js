3b91a59c2647db7e781cb8323466e5add3a90a0c
Brian LeRoux <b@brian.io>
2019-05-30 22:49:38 -0700

    only load env in sandbox

--- src/before/index.js
+++ src/before/index.js
@@ -1,11 +1,9 @@
 let maybeCreate = require('./00-maybe-create')
-let populateEnv = require('./01-populate-env')
-let ensureCreds = require('./02-ensure-creds')
-let printBanner = require('./03-print-banner')
+let ensureCreds = require('./01-ensure-creds')
+let printBanner = require('./02-print-banner')
 
 module.exports = function before() {
   maybeCreate() // maybe create .arc
-  populateEnv() // loads .arc-env into process.env
   ensureCreds() // loads AWS_REGION and AWS_PROFILE
   printBanner() // prints cli banner
 }
